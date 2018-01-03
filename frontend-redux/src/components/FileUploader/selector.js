69a2964cb7f4842a5a56f09128fbc64d741543b5
edvardskazlouski <edward2769940@gmail.com>
2018-01-03 13:30:44 +0300

    delete useless folder

--- frontend-redux/src/components/FileUploader/selector.js
+++
@@ -1,8 +0,0 @@
-import { createStructuredSelector } from 'reselect';
-
-import { loading, files } from 'selectors/fileUploader';
-
-export default createStructuredSelector({
-  loading,
-  files,
-});
