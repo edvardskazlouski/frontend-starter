6ec2660dcbedd324c183493b702843062cfc96b5
Edvards Kazlouski <edvards@plutus.it>
2019-04-15 12:26:39 +0300

    move firebase creds to env

--- frontend-redux/src/config/fbConfig.js
+++ frontend-redux/src/config/fbConfig.js
@@ -2,15 +2,13 @@ import firebase from 'firebase/app';
 import '@firebase/firestore';
 import '@firebase/auth';
 
-// Initialize Firebase
-// Insert your credentials
 export const config = {
-  apiKey: 'AIzaSyBPUuIONtxcQ3efdGpMGbJVEn6fUG_1E5g',
-  authDomain: 'my-first-project-8d8d9.firebaseapp.com',
-  databaseURL: 'https://my-first-project-8d8d9.firebaseio.com',
-  projectId: 'my-first-project-8d8d9',
-  storageBucket: 'my-first-project-8d8d9.appspot.com',
-  messagingSenderId: '801023450889'
+  apiKey: process.env.REACT_APP_FIREBASE_CONFIG_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_CONFIG_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_CONFIG_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_CONFIG_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_CONFIG_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_CONFIG_MSG_SENDER_ID
 };
 
 firebase.initializeApp(config);
